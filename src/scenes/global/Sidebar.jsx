791e4238fc3f9f21cce3b0f73388872630c0729e
jknithin36 <jknithin36@gmail.com>
2024-05-18 17:13:12 -0400

    added demo_images

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/download.jpeg`}
+                  src="../../data/demo.jpeg"
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
